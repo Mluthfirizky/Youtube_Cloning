bf878b42f5ea9d775ce7f377c52b0b1a397c92b8
Mluthfirizky <68719514+Mluthfirizky@users.noreply.github.com>
2021-04-23 13:27:37 +0700

    Add files via upload

---
+++ Yt Clone/Yt Clone/Helper/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Yt Clone
+//
+//  Created by m.luthfi rizky ginting on 29/03/21.
+//
+
+import Foundation
+
+struct Constants {
+    static var API_KEY = "AIzaSyAPsOolYVNzaNhjZjYbvj0uCETWAGaF-bU"
+    static var PLAYLIST_ID = "UUu0yQD7NFMyLu"
+    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    static var VIDEOCEL_ID = "VideoCell"
+    static var YT_EMBED_URL = "https://www.youtube.com/embed/"
+}
+//ini untuk menyimpan link
+
---
+++ Yt Clone/Yt Clone/ViewControllers/ViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ViewController.swift
+//  Yt Clone
+//
+//  Created by m.luthfi rizky ginting on 29/03/21.
+//
+
+import Foundation
